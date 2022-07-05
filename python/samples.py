4feda32b1fe363a4a607117343dffd7cf847aedd
Perry Tew <perry@ultracart.com>
2022-07-05 16:46:02 -0400

    added api creation link

--- python/samples.py
+++ python/samples.py
@@ -5,6 +5,7 @@ from ultracart import ApiClient
 def api_client():
     config = ultracart.Configuration()
     # this key is valid only in the UltraCart development system.  You need to supply a valid simple key here.
+    # Create a Simple Key: https://ultracart.atlassian.net/wiki/spaces/ucdoc/pages/38688545/API+Simple+Key
     config.api_key['x-ultracart-simple-key'] \
         = 'a84dba2b20613c017eff4a1185380100a385a6ff6f6939017eff4a1185380100'
     config.debug = False
--- ruby/constants.rb
+++ ruby/constants.rb
@@ -2,6 +2,7 @@
 
 class Constants
 
+  # Create a Simple Key: https://ultracart.atlassian.net/wiki/spaces/ucdoc/pages/38688545/API+Simple+Key
   API_KEY = 'a84dba2b20613c017eff4a1185380100a385a6ff6f6939017eff4a1185380100'
   VERIFY_SSL = false
   DEBUG_MODE = false
