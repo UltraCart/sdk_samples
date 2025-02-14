779fdf1150dc563e5b974318ffb5bdee5cae6d82
Perry Tew <perry@ultracart.com>
2025-02-14 10:26:40 -0500

    Tax, User, and Workflow php samples (explanation of why we do not have samples for these Internal APIs)

---
+++ php/user/insertUser.php
@@ -0,0 +1,7 @@
+<?php
+// The UserRestAPI is used internally by UltraCart for our own UI.
+// We don't know of a use-case for merchants to use this API in their own automation,
+// but it's a public part of our REST API.
+//
+// We're not including any examples for using the User Api, but if you find yourself needing it for some reason,
+// contact us and we'll assist.  But again, we doubt merchants will ever have a need for this API.
---
+++ php/workflow/getWorkflowAssignmentUsers.php
@@ -0,0 +1,3 @@
+<?php
+// The Workflow REST is used internally by UltraCart for our own UI.
+// We're not including any examples for using the Workflow Api. We doubt merchants will ever have a need for this API.
