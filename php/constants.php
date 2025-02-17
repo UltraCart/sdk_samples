6eb3b3c7e754cebcbf67b0e5d4b73d27368b14f5
Perry Tew <perry@ultracart.com>
2025-02-17 13:48:49 -0500

    PHP - added browser key to Constants

--- php/constants.php
+++ php/constants.php
@@ -4,6 +4,8 @@
     class Constants{
 
         const API_KEY = '506be6208aec1f017c9e54aa881f3700c39c45da408cad017c9e54aa881f3700';
+        const CHANNEL_PARTNER_API_KEY = '5b20522508fe2e01944e96f743a0010052e16518584f1e01944e96f743a00100';
+        const BROWSER_KEY = 'cf286f9201c3b40195153c01cc000100';
         const VERIFY_SSL = false;
         const DEBUG = false;
         const MAX_RETRY_SECONDS = 0;
