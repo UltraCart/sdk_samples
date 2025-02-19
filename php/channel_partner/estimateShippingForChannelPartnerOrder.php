f1abb9cb568e7c9a1f257b15d01e1a3d9af0fc04
Perry Tew <perry@ultracart.com>
2025-02-18 21:52:34 -0500

    PHP - improved estimateShippingForChannelPartnerOrder.php

--- php/channel_partner/estimateShippingForChannelPartnerOrder.php
+++ php/channel_partner/estimateShippingForChannelPartnerOrder.php
@@ -23,16 +23,58 @@ ini_set('display_errors', 1);
 
 use ultracart\v2\api\ChannelPartnerApi;
 use ultracart\v2\models\ChannelPartnerOrder;
+use ultracart\v2\models\ChannelPartnerOrderItem;
+use ultracart\v2\models\ChannelPartnerOrderItemOption;
 
 require_once '../vendor/autoload.php';
 require_once '../constants.php';
 
 
 $channel_partner_api = ChannelPartnerApi::usingApiKey(Constants::CHANNEL_PARTNER_API_KEY);
-$channel_partner_order = new ChannelPartnerOrder();
-// TODO: Populate the channel partner order.  See importChannelPartnerOrder for field examples.
 
-$api_response = $channel_partner_api->estimateShippingForChannelPartnerOrder($channel_partner_order);
+$order = new ChannelPartnerOrder();
+$order->setChannelPartnerOrderId("widget-1245-abc-1");
+$order->setCoupons(["10OFF"]);
+// DeliveryDate will impact shipping estimates if there is a delivery deadline.
+// $order->setDeliveryDate(date('c', strtotime('+14 days')));
+
+$item = new ChannelPartnerOrderItem();
+// $item->setArbitraryUnitCost(9.99);
+// $item->setAutoOrderLastRebillDts(date('c', strtotime('-30 days')));
+// $item->setAutoOrderSchedule("Weekly");
+$item->setMerchantItemId("shirt");
+
+$sizeOption = new ChannelPartnerOrderItemOption();
+$sizeOption->setName("Size");
+$sizeOption->setValue("Small");
+
+$colorOption = new ChannelPartnerOrderItemOption();
+$colorOption->setName("Color");
+$colorOption->setValue("Orange");
+
+$item->setOptions([$sizeOption, $colorOption]);
+$item->setQuantity(1);
+$item->setUpsell(false);
+
+$order->setItems([$item]);
+
+// $order->setShipOnDate(date('c', strtotime('+7 days')));
+$order->setShipToResidential(true);
+$order->setShiptoAddress1("55 Main Street");
+$order->setShiptoAddress2("Suite 202");
+$order->setShiptoCity("Duluth");
+$order->setShiptoCompany("Widgets Inc");
+$order->setShiptoCountryCode("US");
+$order->setShiptoDayPhone("6785552323");
+$order->setShiptoEveningPhone("7703334444");
+$order->setShiptoFirstName("Sally");
+$order->setShiptoLastName("McGonkyDee");
+$order->setShiptoPostalCode("30097");
+$order->setShiptoStateRegion("GA");
+$order->setShiptoTitle("Director");
+
+
+$api_response = $channel_partner_api->estimateShippingForChannelPartnerOrder($order);
 $estimates = $api_response->getEstimates();
 
 // TODO: Apply one estimate shipping method (name) and cost to your channel partner order.
